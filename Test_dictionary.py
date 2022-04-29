9b6cb962808765ccb655a2da190efe3c49b75cb7
PauloRedkva <paulo.redkva@alunos.fael.edu.br>
2022-04-29 10:45:31 -0300

    Adicionando os arquivos do projeto

---
+++ Test_dictionary.py
@@ -0,0 +1,37 @@
+''' starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+
+#starting_dictionary{"c"} = 7
+starting_dictionary["c"] = 7
+final_dictionary = starting_dictionary '''
+
+''' print(final_dictionary)
+
+# --//--
+
+dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+
+print(dict[1]) '''
+
+# ---///----
+
+order = {
+    "starter": {1: "Salad", 2: "Soup"},
+    "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+    "dessert": {1: ["Ice Cream"], 2: []},
+}
+
+print(order["main"][2][0])
